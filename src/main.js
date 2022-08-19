a028e764d852d7953538703ab9dd35c63868ba6c
sifangts <106735204+sifangts@users.noreply.github.com>
2022-08-19 22:39:23 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import App from './App'
 import router from './router'
-import store from './store'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
+
+
 new Vue({
   router,
-  store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
+
